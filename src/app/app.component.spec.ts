dba8c4ad2de75d806a512478c88d2edd9b9c2cd1
lexinelson <lexi.nelson@smoothstack.com>
2021-04-17 19:44:14 -0700

    Refactored project name to angular-ui

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'restaurant-manager-ui'`, () => {
+  it(`should have as title 'angular-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('restaurant-manager-ui');
+    expect(app.title).toEqual('angular-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('restaurant-manager-ui app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-ui app is running!');
   });
 });

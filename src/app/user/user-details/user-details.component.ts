05ae93d968ba25e8c9c48ba64491cb7ce4984264
lexinelson <lexi.nelson@smoothstack.com>
2021-05-09 19:31:40 -0700

    ITB-88: Created details feature with activate/deactivate

--- src/app/user/user-details/user-details.component.ts
+++ src/app/user/user-details/user-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/shared/model/user';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-user-details',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailsComponent implements OnInit {
 
-  constructor() { }
+  user: User
+  success: boolean;
+  message: string;
+  inactive: boolean;
+  activateFailed: boolean;
+
+  constructor(
+    private actRoute: ActivatedRoute,
+    private service: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.message = ''
+    this.service.getUser(Number(this.actRoute.snapshot.paramMap.get("userId"))).then(
+      (resp) => {
+        this.user = resp.body
+        this.inactive = !this.user.isActive
+        this.success = true
+      },
+      (err) => {
+        this.message = `Error: ${err}`
+      }
+    )
+  }
+
+  activate() {
+    console.log("made it to activate")
+    this.user.isActive = true
+    this.service.updateUser(this.user).then(
+      (resp) => {
+        this.user = resp.body
+        this.inactive = !this.user.isActive
+      },
+      (err) => {
+        this.message = `Error: ${err}`
+        this.activateFailed = true
+      }
+    )
+  }
+
+  deactivate() {
+    console.log("made it to deactivate")
+    this.service.deleteUser(Number(this.actRoute.snapshot.paramMap.get("userId"))).then(
+      (resp) => {
+        this.user = resp.body
+        this.inactive = !this.user.isActive
+      },
+      (err) => {
+        this.message = `Error: ${err}`
+        this.activateFailed = true
+      }
+    )
   }
 
 }
